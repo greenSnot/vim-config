e69324128323dca3d418c2e42a7a9a124421c007
t <email@example.com>
2020-03-05 06:17:47 -0500

    prettier

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim_runtime/my_plugins/jedi-vim
+cp ./my_configs.vim ~/.vim_runtime
 
-git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim_runtime/my_plugins/syntastic
+cd ~/.vim_runtime/my_plugins/vim-session
+git clone --recursive https://github.com/davidhalter/jedi-vim.git
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone --depth=1 git@github.com:nikvdp/ejs-syntax.git
 
 # YouCompleteMe
 # tsuquyomi
 # typescript-vim
 # vim-javascript
-git clone -b b-nerdtree-path https://github.com/svanharmelen/vim-session.git ~/.vim_runtime/my_plugins/vim-session
-git clone https://github.com/xolox/vim-misc.git ~/.vim_runtime/my_plugins/vim-misc
-
-cp ./my_configs.vim ~/.vim_runtime
+git clone -b b-nerdtree-path https://github.com/svanharmelen/vim-session.git
+git clone https://github.com/xolox/vim-misc.git
+git clone --depth=1 https://github.com/prettier/vim-prettier
+npm i -g prettier
+cd vim-prettier && npm i && cd ..
--- my_configs.vim
+++ my_configs.vim
@@ -9,10 +9,18 @@ let g:syntastic_check_on_wq = 0
 let g:syntastic_flake8_max_line_length=160
 
 set autochdir
-map <F2> :SaveSession! ~/.vim_session <cr>
-map <F3> :OpenSession! ~/.vim_session <cr>
+map <F2> :SaveSession! .vim_session <cr>
+map <F3> :OpenSession! .vim_session <cr>
 :let g:session_autosave = 'no'
 
 set tabstop=2
 set softtabstop=2
 set shiftwidth=2
+set nu
+
+set mouse=a
+
+set nowrap
+set clipboard=unnamedplus
+
+nmap <Leader>py <Plug>(Prettier)
